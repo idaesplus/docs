6833ab1c18d5fcc4e99ef57e0c3396e7347db78f
Ludovico Bianchi <lbianchi@lbl.gov>
2025-03-22 11:51:36 -0500

    Add files for basic RTD build

---
+++ conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from setuptools_scm import get_version
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'IDAES+'
+copyright = '2025, IDAES+ collaborators'
+author = 'IDAES+ collaborators'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # "sphinx_book_theme",
+    "sphinx_rtd_theme",
+    "myst_parser",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+release = version = get_version(
+    version_scheme="only-version",
+    local_scheme="node-and-date",
+)
+print(f"{version=}")
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ index.md
@@ -0,0 +1,8 @@
+# IDAES+ documentation
+
+This is the unified documentation website for the IDAES+ projects.
+
+```{toctree}
+   :maxdepth: 2
+   :caption: Contents:
+```
